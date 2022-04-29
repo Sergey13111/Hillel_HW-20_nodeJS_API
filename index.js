f594de73accb0619bc27ed1ddea6a18a1bdb2a17
Sergey13111 <boiko.sergey93@gmail.com>
2022-04-29 11:35:09 +0300

    added HW-20

---
+++ index.js
@@ -0,0 +1,8 @@
+const express = require("express");
+const productsRoutes = require("./routes/products");
+
+const PORT = 3000;
+const app = express();
+
+app.use("/api/products", productsRoutes);
+app.listen(PORT);
---
+++ routes/products.js
@@ -0,0 +1,15 @@
+const fs = require("fs");
+const path = require("path");
+const { Router } = require("express");
+
+const router = Router();
+
+router.get("/", (req, res) => {
+    const pathJSON = path.resolve(__dirname, "../data.json")
+    fs.readFile(pathJSON, (err, data) => {
+        console.log(data.toString());
+        res.json(JSON.parse(data));
+    });
+});
+
+module.exports = router;
